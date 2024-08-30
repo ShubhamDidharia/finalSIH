24dcdef7196ffd63214b0da23249e2b82f5972fe
JaiBansal007 <jai.bansal.jai@gmail.com>
2024-08-31 01:00:01 +0530

    added routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,16 +5,17 @@ import Signin from './Component/Signin'
 import { BrowserRouter as Router,Routes,Route, Link } from 'react-router-dom';
 import Signout from './Component/Signout'
 import Spreadsheet from './Component/SpreadSheet';
+import Home from './Component/Home'
 function App() {
   return (
     <>  
-      <Spreadsheet/>
       {/* <Message/> */}
         <Router>
           <Routes>
-            {/* <Route path="/" element={<U/>} /> */}
+            <Route path="/" element={<Home/>}/>
+            <Route path="/login" element={<Signin/>} />
             {/* <Route path="/profile" element={<Profile/>}/> */}
-            {/* <Route path="/sheet" element={<Spreadsheet/>}/> */}
+            <Route path="/sheet" element={<Spreadsheet/>}/>
             {/* <Route path="/login" element={<Signin/>}/> */}
             <Route path="/register" element={<Signout/>}/>
           </Routes>
