887c319804d97f8886fcde6eba26ce5f9fdb7597
JaiBansal007 <jai.bansal.jai@gmail.com>
2024-09-01 04:55:41 +0530

    added last route

--- client/src/Component/Home.jsx
+++ client/src/Component/Home.jsx
@@ -44,7 +44,9 @@ const Home = () => {
     </ul>
   </div>
   <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-      <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none  font-medium rounded-lg text-sm px-4 py-2 text-center">Sign in</button>
+      <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none  font-medium rounded-lg text-sm px-4 py-2 text-center" onClick={()=>{
+        navigate('/login');
+      }}>Sign in</button>
   </div>
   </div>
 </nav>
