b71c4d6339a222b6151c3c032ed24e762f326bc1
JaiBansal007 <jai.bansal.jai@gmail.com>
2024-08-31 17:05:19 +0530

    login route

--- client/src/Component/Login.jsx
+++ client/src/Component/Login.jsx
@@ -70,6 +70,9 @@ const Login = () => {
 
         <button
             type="button"
+            onClick={()=>{
+              navigate("/register");
+            }}
             class=" mb-2 rounded-lg inline-block bg-cyan-500 px-4 pb-2 pt-2.5 text-lg font-semibold leading-normal text-white shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 motion-reduce:transition-none dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong">
             Sign Up
         </button>
