86a7502b6911d34e238c485111b64a8450c490ee
Nayan Jindal <nayan11404@gmail.com>
2024-09-01 18:20:09 +0530

    Sheet

--- public/sheet/socialcalcspreadsheetcontrol.js
+++ public/sheet/socialcalcspreadsheetcontrol.js
@@ -986,10 +986,10 @@ spreadsheet.Buttons = {
          {MouseDown: SocialCalc.DoButtonCmd, command: spreadsheet.Buttons[button].command});
       }
 
-   // create formula bar
-
+   // create formula ba
    spreadsheet.formulabarDiv = document.createElement("div");
    spreadsheet.formulabarDiv.style.height = spreadsheet.formulabarheight + "px";
+   spreadsheet.formulabarDiv.style="margin-bottom: 10px; display: flex; justify-content: center; align-items: center;"
    spreadsheet.formulabarDiv.innerHTML = '<input type="text" size="60" value="">&nbsp;'; //'<textarea rows="4" cols="60" style="z-index:5;background-color:white;position:relative;"></textarea>&nbsp;';
    spreadsheet.spreadsheetDiv.appendChild(spreadsheet.formulabarDiv);
    var inputbox = new SocialCalc.InputBox(spreadsheet.formulabarDiv.firstChild, spreadsheet.editor);
@@ -1000,7 +1000,7 @@ spreadsheet.Buttons = {
       bele.src = spreadsheet.imagePrefix+spreadsheet.formulabuttons[button].image;
       bele.style.verticalAlign = "middle";
       bele.style.border = "1px solid #FFF";
-      bele.style.marginLeft = "4px";
+      bele.style.marginLeft = "14px";
       SocialCalc.TooltipRegister(bele, SCLoc(spreadsheet.formulabuttons[button].tooltip), {});
       SocialCalc.ButtonRegister(bele,
          {normalstyle: "border:1px solid #FFF;backgroundColor:#FFF;",
