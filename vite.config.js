0eb84bc218f7763942e9a4233d57685a0c2d8e12
JaiBansal007 <jai.bansal.jai@gmail.com>
2024-09-01 19:25:34 +0530

    final

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import envCompatible from 'vite-plugin-env-compatible';
-// https://vitejs.dev/config/
+import envCompatible from 'vite-plugin-env-compatible'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    "VITE_",
-    envCompatible()
+    envCompatible({ prefix: "VITE_" }) // Specify the prefix as an option here
   ],
 })
